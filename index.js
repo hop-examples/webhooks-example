10bd21836c27551f9ed8e1ccde9776b41dfd23eb
Cody Miller <hello@looskie.com>
2023-10-02 22:32:04 -0400

    feat: init webhook example

---
+++ index.js
@@ -0,0 +1,45 @@
+import "dotenv/config";
+
+import express from "express";
+import { constructEvent } from "@onehop/js";
+
+// generate yours at https://console.hop.io/project/settings/webhooks
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET ?? "whsec_xxxx";
+const PORT = process.env.PORT ?? 3001;
+
+const app = express();
+app.use(
+  express.json({
+    // Add the rawBody string to the request object
+    verify: (req, _res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
+
+app.get("/", (_req, res) => res.send("hello world"));
+app.post("/webhook", async (req, res) => {
+  try {
+    // If you don't want to use our event constructor, you can always use verifyHmac utility function from @onehop/js.
+    const event = await constructEvent(
+      req.rawBody,
+      req.headers["x-hop-hooks-signature"],
+      WEBHOOK_SECRET
+    );
+
+    console.log(
+      `Received event: ${event.id} from ${event.webhook_id}\n${
+        event.event
+      } with ${JSON.stringify(event.data)}`
+    );
+
+    res.status(204);
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send("invalid hmac");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
